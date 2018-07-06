5ecf9787636c8e958962fe4a66cc25e410f780f0
modolee <lsi1318@gmail.com>
2018-07-06 19:45:56 +0900

    주석 추가

--- contracts/Reputation.sol
+++ contracts/Reputation.sol
@@ -3,39 +3,45 @@ pragma solidity ^0.4.24;
 contract Reputation {
     enum Grade {VeryGood, Good, Normal, NotBad, Bad}
 
+    // 평판 정보
     struct ReputationInfo {
         address writer;
         string comment;
         Grade grade;
     }
 
+    // 호스트 평판
     mapping (address => ReputationInfo[]) hostReputation;
+    // 게스트 평판
     mapping (address => ReputationInfo[]) guestReputation;
 
+    // 호스트 평판 등록
     function setHostReputation(
         address _host,
         string _comment,
         uint8 _grade
     )
-    public
+        public
     {
-        ReputationInfo memory reputation
-        = ReputationInfo(msg.sender, _comment, Grade(_grade));
+        require(_host != msg.sender);
+        ReputationInfo memory reputation = ReputationInfo(msg.sender, _comment, Grade(_grade));
         hostReputation[_host].push(reputation);
     }
 
+    // 게스트 평판 등록
     function setGuestReputation(
         address _guest,
         string _comment,
         uint8 _grade
     )
-    public
+        public
     {
-        ReputationInfo memory reputation
-        = ReputationInfo(msg.sender, _comment, Grade(_grade));
+        require(_guest != msg.sender);
+        ReputationInfo memory reputation = ReputationInfo(msg.sender, _comment, Grade(_grade));
         guestReputation[_guest].push(reputation);
     }
 
+    // 등록 된 호스트 평판 개수
     function getHostReputationNumber(address _host)
     public
     view
@@ -44,6 +50,7 @@ contract Reputation {
         return hostReputation[_host].length;
     }
 
+    // 등록 된 게스트 평판 개수
     function getGuestReputationNumber(address _guest)
     public
     view
@@ -52,6 +59,7 @@ contract Reputation {
         return guestReputation[_guest].length;
     }
 
+    // 등록 된 호스트 평판 읽기
     function getHostReputation(address _host, uint256 _id)
     public
     view
@@ -61,6 +69,7 @@ contract Reputation {
         return (reputation.writer, reputation.comment, reputation.grade);
     }
 
+    // 등록 된 게스트 평판 읽기
     function getGuestReputation(address _guest, uint256 _id)
     public
     view
