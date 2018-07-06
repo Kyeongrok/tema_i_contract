6881072629d64ede96ead7f93e50356a8f02a8d9
Jeff <woo@titaniumcodes.com>
2018-07-06 21:18:48 +0900

    fix reservation test to mint before reserve.

--- test/reservetoken.js
+++ test/reservetoken.js
@@ -1,24 +1,29 @@
 const Room = artifacts.require("./Room.sol");
 const Reservation = artifacts.require("./Reservation.sol");
+const TemaToken = artifacts.require("./TemaToken.sol");
 
-contract('Room', (accounts) => {
+contract('Reservation', (accounts) => {
     it("reserve", () => {
-        return Room.deployed().then(instance => {
-            // getRoom()
-            return Room.deployed().then(instance => {
-                instance.registRoom("room-1", 100);
-                instance.roomCount().then(count => console.log("roomCount:",count));
-                instance.myAccount().then(addr => {
-                    return Reservation.deployed().then(rc =>{
-                        rc.reserve(addr, "7-5", 1);
-                        rc.reserves(addr).then(item=>{
-                            assert.equal('7-5', item[1]);
-                        })
-                    });
-                });
-        });
+        return Reservation.deployed().then(instance => {
+            return instance.registRoom("room-1", 100).then(() => {
 
-    });
+                instance.roomCount().then(count => console.log("roomCount:",count));
+                return instance.myAccount().then(addr => {
+
+                    return TemaToken.deployed().then(tema => {
+                        tema.mint(addr, 100000);
+                        return instance.reserve(addr, "7-5", 2).then(() => {
+                            return instance.reserves(addr).then(item=>{
+                                assert.equal('7-5', item[1]);
+                                assert.equal(200, item[3]);
+                            })
+                        });
+                    });
+
+                });
+            });
+
+        });
     });
 
 
