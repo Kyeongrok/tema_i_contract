0dcf3026e07d1f5686108ecd12bd937bf5f6402a
kyeongrok <kyeongrok.kim@okiconcession.com>
2018-07-06 11:07:08 +0900

    test code 작성 account를 echo하는 code추가

--- test/reservetoken.js
+++ test/reservetoken.js
@@ -1,17 +1,27 @@
 const ReserveToken = artifacts.require("./ReserveToken.sol");
 
 contract('ReserveToken', (accounts) => {
-    it("first case", () => {
-        ReserveToken.deployed().then((instance) => {
-
-            console.log(accounts[0]);
-
-            instance.registRoom("hello", 100);
-            instance.echoAddress(accounts[0]).then((item)=>{
+    it("echo address", () => {
+        return ReserveToken.deployed().then(instance => {
+            instance.echoAccount(accounts[0]).then((item)=>{
                 console.log(item);
+                assert.equal(accounts[0], item);
             });
-
         });
-
     });
+
+    it("reserve and count", () => {
+        return ReserveToken.deployed().then((instance) => {
+            instance.reserve(accounts[0], "7-1", "7-2")
+                .then(result => {
+                    return instance.roomCount();
+                })
+                .then(count => {
+                    console.log(`count: ${count}`)
+                    assert.equal(0, count);
+                });
+        });
+    });
+
+
 });
