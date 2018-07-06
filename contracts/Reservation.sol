657d7e5fd2d1e7fa2a0f663b120c7cf2785b7ff9
Jeff <woo@titaniumcodes.com>
2018-07-06 16:49:31 +0900

    add totalPrice into ReservationInfo.

--- contracts/Reservation.sol
+++ contracts/Reservation.sol
@@ -6,6 +6,8 @@ import "./TemaTokenMarket.sol";
 import "./Reputation.sol";
 
 contract Reservation is Room, Reputation {
+    using SafeMath for uint256;
+
     enum ReservationState { Reserved, CheckedIn, CheckedOut }
 
     mapping(address => ReservationInfo) public reserves;
@@ -16,6 +18,7 @@ contract Reservation is Room, Reputation {
         address host;
         string from;
         uint duration;
+        uint256 totalPrice;
         ReservationState state;
     }
 
@@ -27,9 +30,16 @@ contract Reservation is Room, Reputation {
 
     function reserve(address _host, string _from, uint _duration) public{
         address guest = msg.sender;
+
+        RoomInfo memory room = rooms[_host];
+        uint totalPrice = room.price * _duration;
+
+        require(temaToken.balanceOf(guest) >= totalPrice);
+
         ReservationInfo storage reservation = reserves[guest];
         reservation.host = _host;
         reservation.from = _from;
+        reservation.totalPrice = totalPrice;
         // reservation.state = ReservationState.Reserved; //우선 체크인상태로 바로 설정.
         reservation.state = ReservationState.CheckedIn;
         reservation.duration = _duration;
@@ -39,17 +49,17 @@ contract Reservation is Room, Reputation {
 
     function checkout() public {
         ReservationInfo storage reservation = reserves[msg.sender];
-
         // 체크인 상태인지 확인
         require(reservation.state == ReservationState.CheckedIn);
-
-        // 호스트에게 토큰 전달
-
         // 체크아웃으로 변경
         reservation.state = ReservationState.CheckedOut;
     }
 
-    function claim(address _from, uint256 _value) public {
-        temaToken.transferFrom(_from, msg.sender, _value);
+    function claim(address _from) public {
+        ReservationInfo storage reservation = reserves[_from];
+        // 체크아웃 상태인지 확인
+        require(reservation.state == ReservationState.CheckedOut);
+        // 호스트에게 토큰 전달
+        temaToken.transferFrom(_from, msg.sender, reservation.totalPrice);
     }
 }
